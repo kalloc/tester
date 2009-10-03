327ec88b42acdecd4a06952747ed52a9f87657fd
nick <nick@a7f69714-1af5-46c3-87cb-08706bf1f93a>
2009-10-03 00:35:36 +0000

    добавлена поддержка файловых логов испавлена ошибка с подстройкой времени проверки
    git-svn-id: svn://track.redmine.ru/monitoring/tester@14 a7f69714-1af5-46c3-87cb-08706bf1f93a

--- report.c
+++ report.c
@@ -4,16 +4,10 @@ Server *pServerDC = NULL;
 struct ReportListHead *Head; /* List head. */
 static pthread_mutex_t *send_mutex = NULL;
 
-/*
- * добавляем репорт в очередь
- * ReportError  - обрабатывается раз в секунду
- * Report       - как происходит обновление данных с сервера
- */
-
 void addICMPReport(struct Task * task) {
-    static pthread_mutex_t *mutex=NULL;
+    static pthread_mutex_t *mutex = NULL;
     if (!mutex) {
-        mutex=calloc(1,sizeof(*mutex));
+        mutex = calloc(1, sizeof (*mutex));
         pthread_mutex_init(mutex, NULL);
     }
     pthread_mutex_lock(mutex);
@@ -24,9 +18,7 @@ void addICMPReport(struct Task * task) {
 
     incStat(task->Record.ModType, task->code);
 
-#ifdef DEBUG
-    printf(cGREEN"REPORT"cEND" ADD -> id %d, Module %s for %s [%s:%d]\n", task->Record.LObjId, getModuleText(task->Record.ModType), task->Record.HostName, ipString(task->Record.IP), task->Record.Port);
-#endif
+
     ReportEntryPtr = getNulledMemory(sizeof (struct ReportListEntry)); /* Insert at the head. */
 
     ReportEntryPtr->ModType = MODULE_PING;
@@ -40,6 +32,14 @@ void addICMPReport(struct Task * task) {
     ping->ProblemIP = poll->ProblemIP;
     ping->ProblemICMP_Code = poll->ProblemICMP_Code;
     ping->ProblemICMP_Type = poll->ProblemICMP_Type;
+    debug("%d -> Module %s for %s [%s:%d] [ms: %d] [time test:%d] Result %s",
+            task->Record.LObjId,
+            getModuleText(task->Record.ModType),
+            task->Record.HostName, ipString(task->Record.IP), task->Record.Port,
+            poll->DelayMS,
+            poll->CheckDt,
+            getStatusText(task->code));
+
     ReportEntryPtr->Data = (char *) ping;
 
     if (pServerDC) {
@@ -81,7 +81,7 @@ void addICMPReport(struct Task * task) {
 void addTCPReport(struct Task * task) {
     static pthread_mutex_t *mutex;
     if (!mutex) {
-        mutex=calloc(1,sizeof(*mutex));
+        mutex = calloc(1, sizeof (*mutex));
         pthread_mutex_init(mutex, NULL);
     }
     pthread_mutex_lock(mutex);
@@ -106,9 +106,13 @@ void addTCPReport(struct Task * task) {
     tcp->ProblemICMP_Type = poll->ProblemICMP_Type;
     tcp->Port = task->Record.Port;
 
-#ifdef DEBUG
-    printf(cGREEN"REPORT"cEND" ADD -> id %d,State is %s, Module %s for %s [%s:%d] [ms: %d] [time test:%d]\n", task->Record.LObjId, getStatusText(task->code), getModuleText(task->Record.ModType), task->Record.HostName, ipString(task->Record.IP), task->Record.Port, tcp->DelayMS, tcp->CheckDt);
-#endif
+    debug("%d -> Module %s for %s [%s:%d] [ms: %d] [time test:%d] Result %s",
+            task->Record.LObjId,
+            getModuleText(task->Record.ModType),
+            task->Record.HostName, ipString(task->Record.IP), task->Record.Port,
+            tcp->DelayMS,
+            tcp->CheckDt,
+            getStatusText(task->code));
 
     ReportEntryPtr->Data = (char *) tcp;
 
@@ -173,9 +177,6 @@ void _SendReport(Server *pServer, short error) {
 #ifdef ONEPACKET
 
     LIST_FOREACH(ReportEntryPtr, Head, entries) {
-#ifdef DEBUG
-        printf("\tPrepare to send LObjId %d, result %s \n", *(int*) ReportEntryPtr->Data, error ? "ERROR" : "OK");
-#endif
         if (ptr[ReportEntryPtr->ModType] == 0) {
             ptr[ReportEntryPtr->ModType] = evbuffer_new();
         }
@@ -208,40 +209,26 @@ void _SendReport(Server *pServer, short error) {
 };
 
 void SendReport(Server *pServer) {
-#ifdef DEBUG
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    printf(cGREEN"REPORT"cEND" SEND %d, sec is %d[%d] %s:%d \n", pServer->report.counterReport, tv.tv_sec - config.TimeStabilization, config.TimeStabilization, pServer->host, pServer->port);
-#endif
+    debug("%s:%d -> Send %d report", pServer->host, pServer->port, pServer->report.counterReport);
     if (pServer->report.counterReport == 0)return;
     _SendReport(pServer, FALSE);
     pServer->report.counterReport = 0;
     pServer->flagSendReport = 0;
-#ifdef DEBUG
-    printf("\n");
-#endif
 
 }
 
 void SendReportError(Server *pServer) {
-#ifdef DEBUG
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    printf(cGREEN"REPORT ERROR"cEND" SEND %d, sec is %d[%d] %s:%d \n", pServer->report.counterReportError, tv.tv_sec - config.TimeStabilization, config.TimeStabilization, pServer->host, pServer->port);
-#endif
+    debug("%s:%d -> Send %d report", pServer->host, pServer->port, pServer->report.counterReportError);
     if (pServer->report.counterReportError == 0) return;
     _SendReport(pServer, TRUE);
     pServer->report.counterReportError = 0;
     pServer->flagSendReportError = 0;
-#ifdef DEBUG
-    printf("\n");
-#endif
 
 }
 
 void initReport(Server *pServer) {
     if (!send_mutex) {
-        send_mutex=calloc(1,sizeof(*send_mutex));
+        send_mutex = calloc(1, sizeof (*send_mutex));
         pthread_mutex_init(send_mutex, NULL);
     }
     if (pServer->isDC) {
