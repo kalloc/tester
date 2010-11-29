7d570197fee3b6ae4ee44878a26682cbb4891dc2
root <root@mail.asprex.ru>
2010-11-29 20:41:02 +0300

    custom deps builder

---
+++ sh/deps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export CPPFLAGS='-O0 -ggdb'
+
+echo 'Install some deps Libs:'
+cd ../deps;
+find -maxdepth 1 -type f -iname "*.sh" | while read file;do
+    name=${file/.sh/}
+    echo -ne "\t${name}: Unpack"
+    tar xf ${name}.tar.gz
+    cd ./${name}/
+    ../${file}
+    cd ../ 
+    echo ' and Clean'
+    rm -rf ./${name}
+    touch ${name}.installed
+done
+echo 'Done, ready to make tester'
