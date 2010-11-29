e7626b8e918e63d6ab51c9d9bf158d06cf109cda
root <root@mail.asprex.ru>
2010-11-29 22:05:08 +0300

    custom deps builder

--- sh/setup_tester.sh
+++ sh/setup_tester.sh
@@ -14,10 +14,10 @@ function red() {
 }
 
 
-if [ -d /opt/tester ];then
-    red тестер уже стоит, но мы не будем ебать вам мозг и попробуем обновить
+if [ "$1" == "lua" ];then
+    red обновляем только lua скрипты
     echo "::exec wget daedalus.ru/tester.tar -qO - | tar xf - -C /opt/tester/ ./tester ./lua"
-    wget daedalus.ru/tester.tar -qO - | tar xf - -C /opt/tester/ ./tester ./lua
+    wget daedalus.ru/tester.tar -qO - | tar xf - -C /opt/tester/ ./lua
     green "обновили, теперь перезапустим по жесткому =("
     echo "::exec killall -9 tester"
     killall -9 tester
@@ -33,10 +33,10 @@ function makelink () {
 
 green 1* скачиваем нужные пакеты
 if [ -f /etc/debian_version ];then
-    apt-get install openssl screen -y gdb python-xmpp  libssl-dev g++ make libxml2-dev
+    apt-get install openssl screen -y gdb python-xmpp  libssl-dev g++ make libxml2-dev -qq  >/dev/null 2>&1
 elif [ -f /etc/redhat-release ];then
-    rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-3.noarch.rpm
-    yum --noplugins install -y libevent libevent-devel mc zlib openssl-devel  strace lua valgrind nmap  gdb python-xmpp openssl screen
+    rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-3.noarch.rpm  >/dev/null 2>&1
+    yum --noplugins install -y mc zlib openssl-devel  strace valgrind nmap  gdb python-xmpp openssl screen  >/dev/null 2>&1
     if [ -f /usr/lib/libssl.so.6 ];then
 	path=/usr/lib/
     elif [ -f /lib/libssl.so.6 ];then
@@ -60,7 +60,7 @@ fi
 
 green 2* создаём окружение
 mkdir /opt/tester/;
-wget daedalus.ru/tester.tar -qO - | tar xf - -C /opt/tester/
+wget daedalus.ru/tester.tar.gz -qO - | tar zxf - -C /opt/tester/ >/dev/null 2>&1
 
 
 
@@ -74,9 +74,13 @@ grep tester/log /etc/crontab   >/dev/null|| echo -ne  "0 3 * * * root find /opt/
 
 green 5* install deps
 echo 'Install some deps Libs:'
-cd ./deps;
+cd /opt/tester/deps;
 find -maxdepth 1 -type f -iname "*.sh" | while read file;do
     name=${file/.sh/}
+    if  [ -f ${name}.installed ];then
+	echo -ne "\t${name}: installed. skip\n"
+	continue
+    fi
     echo -ne "\t${name}: Unpack"
     tar xf ${name}.tar.gz
     cd ./${name}/
@@ -84,13 +88,16 @@ find -maxdepth 1 -type f -iname "*.sh" | while read file;do
     cd ../ 
     echo ' and Clean'
     rm -rf ./${name}
+    touch ${name}.installed
 done
-cd ../
 echo 'Done, ready to make tester'
-green 6* make
-gcc -I/opt/tester/include/ -I/usr/include/libxml2/    -o tester  aes/aes.c curve25519-donna/curve25519-donna.c *.c  -L/opt/tester/lib/ -lz -lssl -lm /opt/tester/lib/liblua.a /opt/tester/lib/libevent.a -lpthread -lrt /opt/tester/lib/libevent_pthreads.a /opt/tester/lib/libcares.a -lxml2
+green 6* компилируем
+cd /opt/tester
+gcc -I/opt/tester/include/ -I/usr/include/libxml2/    -o tester  aes/aes.c curve25519-donna/curve25519-donna.c *.c  -L/opt/tester/lib/ -lz -lssl -lm /opt/tester/lib/liblua.a /opt/tester/lib/libevent.a -lpthread -lrt /opt/tester/lib/libevent_pthreads.a /opt/tester/lib/libcares.a -lxml2  > /dev/null 2>&1
 
-green 7* hands job
+green 7* удаляем исходный код
+rm -rf /opt/tester/aes/ /opt/tester/curve25519-donna/ /opt/tester/*.c  /opt/tester/*.h /opt/tester/Makefile 
+green 8* hands job
 
 echo пробуем запустить /opt/tester/tester если получилось то запускаем в бэкраунде screen -dmS tester /opt/tester/start.sh
 
